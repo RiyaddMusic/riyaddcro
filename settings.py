21ac98c83dccc89d68de7888779f6d2651449f96
DCMMusic <102096177+DCMMusic@users.noreply.github.com>
2022-06-10 02:15:03 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5030510796:AAEUDLAlcAjbCMTORkR5LZzr9_QW-am5MBo"
+TOKEN = "5455855733:AAEvqOAfig2fowa2rdDujSpU2fdHF5cSvbc"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
