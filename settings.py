52c3abb5380de7ce49a3278b7da1379bebfeb3fe
RiyaddMusic <106576944+RiyaddMusic@users.noreply.github.com>
2023-02-22 11:01:09 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5455855733:AAEvqOAfig2fowa2rdDujSpU2fdHF5cSvbc"
+TOKEN = "5693448468:AAEHyxu1OggskH2ditNaH0l9M9HuFAdfQLc"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
